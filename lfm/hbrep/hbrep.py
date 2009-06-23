6005fb461455561bb29b22994c2065a4ae5b376b
Tim Sell <trsell@gmail.com>
2009-06-23 23:29:10 +0100

    wrong arg

--- lfm/hbrep/hbrep.py
+++ lfm/hbrep/hbrep.py
@@ -47,7 +47,7 @@ class HBaseReplic(skytools.DBScript):
       if cmd == "play":
         self.run_script = HBaseConsumer(service_name, self.config_file)
       elif cmd == "bootstrap":
-        self.run_script = HBaseBootstrap(service_name, [self.config_file] + self.table_names)
+          self.run_script = HBaseBootstrap(service_name, [self.config_file] + args[2:])
       #elif cmd == "install":
       #  self.work = self.do_install
       #elif cmd == "uninstall":
